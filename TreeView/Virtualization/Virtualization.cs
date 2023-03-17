82168a82c7fd51c7385eb8f35c774100d7fe915e
Тимур Толстов <tima.tolstow@mail.ru>
2023-03-17 16:33:54 +0300

    CollectionView

---
+++ TreeView/Virtualization/Virtualization.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreeView.Controls.TreeView.Models;
+
+namespace TreeView.Virtualization
+{
+    class Virtualization
+    {
+        public void RemoveCollectionItem(IEnumerable Collection, int positionid)
+        {
+
+        }
+        public void AddCollectionItem(IEnumerable Collection, int positionid)
+        {
+
+        }
+        public void GroupColletion(IEnumerable Collection)
+        {
+
+        }
+        public IEnumerable VirtualizeItems(IEnumerable Collection, string action)
+        {
+            var collection = Collection;
+            if(action == "Down")
+            {
+                RemoveCollectionItem(collection, 0);
+                AddCollectionItem(collection, 0);
+            }
+            return collection;
+        }
+    }
+}
