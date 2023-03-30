8ef1df6d2e7fc1cc1ce7479c9c55eb12aa2a0b17
Тимур Толстов <tima.tolstow@mail.ru>
2023-03-30 21:34:29 +0300

    fix

--- TreeView/Controls/CustomTreeView/CustomTree/TreeData.cs
+++ TreeView/Controls/CustomTreeView/CustomTree/TreeData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Maui.Controls.Internals;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -10,14 +11,21 @@ namespace TreeView.Controls.CustomTreeView.CustomTree
 {
     public class TreeData
     {
+
+        public Command LoadChilds { get; set; }
         public ObservableCollection<NodeTree> nodeTrees { get; set; } = new();
         public TreeData()
         {
-            
+            InitAsync();
+            LoadChilds = new Command(async (object args) => await OpenChilds(args as NodeTree));
         }
-        public async Task LoadNodes()
+        private async void InitAsync()
         {
-            List<NodeTree> nodes =  LoadTree();
+           await LoadNodes();
+        }
+        private async Task LoadNodes()
+        {
+            List<NodeTree> nodes =  await LoadTree();
             if(nodes!= null)
             {
                 foreach (NodeTree node in nodes)
@@ -27,13 +35,56 @@ namespace TreeView.Controls.CustomTreeView.CustomTree
             }
             else
             {
-                Shell.Current.DisplayAlert("data error","data is null","ok");
+                await Shell.Current.DisplayAlert("Nodes error","Nodes is null","ok");
             }
         }
-        public virtual List<NodeTree> LoadTree()
+        private async Task OpenChilds(NodeTree nodeTree)
         {
-            List<NodeTree> nodeTrees = new List<NodeTree>();
-            return nodeTrees;
+            int index = nodeTrees.IndexOf(nodeTree);
+
+            if (nodeTree.ChildElements == null)
+            {
+                var ListChilds = await LoadChildsFunctions(nodeTree);
+
+                foreach (NodeTree node in ListChilds)
+                {
+                    nodeTrees.Insert(index + 1, node);
+                    nodeTree.ChildElements.Add(node);
+                }
+                nodeTree.IsVisibleChildElements = true;
+            }
+            else
+            {
+                if (nodeTree.IsVisibleChildElements)
+                {
+                    foreach (var node in nodeTree.ChildElements)
+                    {
+                        var nodetohide = nodeTrees.FirstOrDefault(p => p.UniqueId == node.UniqueId);
+                        nodetohide.IsVisible = false;
+                    }
+                    nodeTree.IsVisibleChildElements = false;
+                }
+                else
+                {
+                    foreach (var node in nodeTree.ChildElements)
+                    {
+                        var nodetohide = nodeTrees.FirstOrDefault(p => p.UniqueId == node.UniqueId);
+                        nodetohide.IsVisible = true;
+                    }
+                    nodeTree.IsVisibleChildElements = true;
+                }
+            }
+
         }
+        public async virtual Task<List<NodeTree>> LoadTree()
+        {
+            return null;
+        }
+        public async virtual Task<List<NodeTree>> LoadChildsFunctions(NodeTree nodeTree)
+        {
+            return null;
+        }
+        
+       
     }
 }
